83a30f9b1da680265979206abbebef57c3a92b0a
Felipe <felipemelozx@gmail.com>
2024-05-16 14:24:16 -0300

    H2 database, test profile, jpa

--- src/main/java/com/felipemelozx/course/entities/User.java
+++ src/main/java/com/felipemelozx/course/entities/User.java
@@ -1,10 +1,20 @@
 package com.felipemelozx.course.entities;
 
+import jakarta.persistence.*;
+
 import java.io.Serializable;
 import java.util.Objects;
 
+
+@Entity
+@Table(name = "tb_user")
 public class User implements Serializable {
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private  long id;
+
     private  String name;
     private  String email;
     private  String phone;
