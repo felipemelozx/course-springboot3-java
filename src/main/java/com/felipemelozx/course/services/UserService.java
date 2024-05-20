99041d00cc14b5915987f5f2a22ab5484e982120
Felipe <felipemelozx@gmail.com>
2024-05-20 15:04:48 -0300

    exception handling delete

--- src/main/java/com/felipemelozx/course/services/UserService.java
+++ src/main/java/com/felipemelozx/course/services/UserService.java
@@ -3,8 +3,11 @@ package com.felipemelozx.course.services;
 
 import com.felipemelozx.course.entities.User;
 import com.felipemelozx.course.repositories.UserRepository;
+import com.felipemelozx.course.services.exception.DatabaseException;
 import com.felipemelozx.course.services.exception.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -31,7 +34,13 @@ public class UserService {
     }
 
     public void delete(Long id){
-        repository.deleteById(id);
+        try{
+            repository.deleteById(id);
+        }catch (EmptyResultDataAccessException e){
+        throw new ResourceNotFoundException(id);
+        }catch (DataIntegrityViolationException e){
+            throw new DatabaseException(e.getMessage());
+        }
     }
 
     public User update(Long id, User obj){
