3e8e1813fb7ab2eb8366fcc00ef2663d37e49b01
Felipe <felipemelozx@gmail.com>
2024-05-20 13:56:35 -0300

    User Delete

--- src/main/java/com/felipemelozx/course/resources/UserResource.java
+++ src/main/java/com/felipemelozx/course/resources/UserResource.java
@@ -36,4 +36,9 @@ public class UserResource {
                 .buildAndExpand(obj.getId()).toUri();
         return ResponseEntity.created(uri).body(obj);
     }
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity<Void> deleteById(@PathVariable Long id){
+        service.delete(id);
+        return ResponseEntity.noContent().build();
+    }
 }
